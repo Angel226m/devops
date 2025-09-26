0aa66339da5b975d9979e44d5747d1f2ed314d33
Angel226m <2201010137@undc.edu.pe>
2025-09-25 21:38:21 -0500

    implementacion de detalles d ereserva en pagian cliente

--- paginawebbtour/src/infraestructura/ui/rutas/RutasPublicas.tsx
+++ paginawebbtour/src/infraestructura/ui/rutas/RutasPublicas.tsx
@@ -3,67 +3,6 @@
 import { Routes, Route } from 'react-router-dom';
 import { lazy } from 'react';
 import RutasPrivadas from './RutasPrivadas';
-
-// Importaciones lazy loading para páginas públicas
-const PaginaInicio = lazy(() => import('../paginas/publicas/PaginaInicio'));
-const PaginaTours = lazy(() => import('../paginas/publicas/PaginaTours'));
-const PaginaDetalleTour = lazy(() => import('../paginas/publicas/PaginaDetalleTour'));
-const PaginaSedes = lazy(() => import('../paginas/publicas/PaginaSedes'));
-const PaginaSobreNosotros = lazy(() => import('../paginas/publicas/PaginaSobreNosotros'));
-const PaginaContacto = lazy(() => import('../paginas/publicas/PaginaContacto'));
-const PaginaPreguntas = lazy(() => import('../paginas/publicas/PaginaPreguntas'));
-const PaginaIngreso = lazy(() => import('../paginas/publicas/PaginaIngreso'));
-const PaginaRegistro = lazy(() => import('../paginas/publicas/PaginaRegistro'));
-const PaginaRecuperacion = lazy(() => import('../paginas/publicas/PaginaRecuperacion'));
-const PaginaNoEncontrada = lazy(() => import('../paginas/publicas/PaginaNoEncontrada'));
-
-// Importaciones lazy loading para páginas privadas
-const PaginaProcesoPago = lazy(() => import('../paginas/privadas/PaginaProcesoPago'));
-const PaginaPerfil = lazy(() => import('../paginas/privadas/PaginaPerfil'));
-const PaginaReservasUsuario = lazy(() => import('../paginas/privadas/PaginaReservasUsuario'));
-
-// Importaciones para las nuevas páginas de resultado de pago
-const PaginaReservaExitosa = lazy(() => import('../paginas/privadas/PaginaReservaExitosa'));
-const PaginaPagoFallido = lazy(() => import('../paginas/privadas/PaginaPagoFallido'));
- 
-const RutasPublicas = () => {
-  return (
-    <Routes>
-      {/* Rutas públicas *//*}
-      <Route path="inicio" element={<PaginaInicio />} />
-      <Route path="tours" element={<PaginaTours />} />
-      <Route path="tours/:idTour" element={<PaginaDetalleTour />} />
-      <Route path="sedes" element={<PaginaSedes />} />
-      <Route path="sobre-nosotros" element={<PaginaSobreNosotros />} />
-      <Route path="contacto" element={<PaginaContacto />} />
-      <Route path="preguntas-frecuentes" element={<PaginaPreguntas />} />
-      <Route path="ingresar" element={<PaginaIngreso />} />
-      <Route path="registrarse" element={<PaginaRegistro />} />
-      <Route path="recuperar-contrasena" element={<PaginaRecuperacion />} />
-      
-      {/* Rutas privadas protegidas por autenticación *//*}
-      <Route element={<RutasPrivadas />}>
-        <Route path="proceso-pago" element={<PaginaProcesoPago />} />
-        <Route path="perfil" element={<PaginaPerfil />} />
-        <Route path="mis-reservas" element={<PaginaReservasUsuario />} />
-        
-        {/* Nuevas rutas para los resultados de pago *//*}
-        <Route path="reserva-exitosa" element={<PaginaReservaExitosa />} />
-        <Route path="pago-fallido" element={<PaginaPagoFallido />} />
-      </Route>
-       
-      {/* Ruta de fallback para rutas no encontradas *//*}
-      <Route path="*" element={<PaginaNoEncontrada />} />
-    </Routes>
-  );
-};
-
-export default RutasPublicas;*/
-
-
-import { Routes, Route } from 'react-router-dom';
-import { lazy } from 'react';
-import RutasPrivadas from './RutasPrivadas';
 const PaginaConservacion = lazy(() => import('../paginas/publicas/PaginaConservacion'));
 // Importaciones lazy loading para páginas públicas
 const PaginaInicio = lazy(() => import('../paginas/publicas/PaginaInicio'));
@@ -96,7 +35,7 @@ const PaginaPagoFallido = lazy(() => import('../paginas/privadas/PaginaPagoFalli
 const RutasPublicas = () => {
   return (
     <Routes>
-      {/* Rutas públicas */}
+      {/* Rutas públicas *//*}
       <Route path="inicio" element={<PaginaInicio />} />
       <Route path="tours" element={<PaginaTours />} />
       <Route path="tours/:idTour" element={<PaginaDetalleTour />} />
@@ -109,19 +48,101 @@ const RutasPublicas = () => {
       <Route path="ingresar" element={<PaginaIngreso />} />
       <Route path="registrarse" element={<PaginaRegistro />} />
       <Route path="recuperar-contrasena" element={<PaginaRecuperacion />} />
-      <Route path="cambiar-contrasena" element={<PaginaCambioContrasena />} /> {/* Añadida esta ruta */}
+      <Route path="cambiar-contrasena" element={<PaginaCambioContrasena />} /> {/* Añadida esta ruta *//*}
       
-       {/* Rutas para páginas legales */}
+       {/* Rutas para páginas legales *//*}
       <Route path="terminos" element={<PaginaTerminosCondiciones />} />
       <Route path="privacidad" element={<PaginaPoliticaPrivacidad />} />
       <Route path="cookies" element={<PaginaPoliticaCookies />} />
-      {/* Rutas privadas protegidas por autenticación */}
+      {/* Rutas privadas protegidas por autenticación *//*}
       <Route element={<RutasPrivadas />}>
         <Route path="proceso-pago" element={<PaginaProcesoPago />} />
         <Route path="perfil" element={<PaginaPerfil />} />
         <Route path="mis-reservas" element={<PaginaReservasUsuario />} />
         
-        {/* Nuevas rutas para los resultados de pago */}
+        {/* Nuevas rutas para los resultados de pago *//*}
+        <Route path="reserva-exitosa" element={<PaginaReservaExitosa />} />
+        <Route path="pago-fallido" element={<PaginaPagoFallido />} />
+      </Route>
+       
+      {/* Ruta de fallback para rutas no encontradas *//*}
+      <Route path="*" element={<PaginaNoEncontrada />} />
+    </Routes>
+  );
+};
+
+export default RutasPublicas;*/
+
+
+import { Routes, Route } from 'react-router-dom';
+import { lazy } from 'react';
+import RutasPrivadas from './RutasPrivadas';
+
+const PaginaConservacion = lazy(() => import('../paginas/publicas/PaginaConservacion'));
+
+// Importaciones lazy loading para páginas públicas
+const PaginaInicio = lazy(() => import('../paginas/publicas/PaginaInicio'));
+const PaginaTours = lazy(() => import('../paginas/publicas/PaginaTours'));
+const PaginaDetalleTour = lazy(() => import('../paginas/publicas/PaginaDetalleTour'));
+const PaginaSedes = lazy(() => import('../paginas/publicas/PaginaSedes'));
+const PaginaSobreNosotros = lazy(() => import('../paginas/publicas/PaginaSobreNosotros'));
+const PaginaContacto = lazy(() => import('../paginas/publicas/PaginaContacto'));
+const PaginaPreguntas = lazy(() => import('../paginas/publicas/PaginaPreguntas'));
+const PaginaIngreso = lazy(() => import('../paginas/publicas/PaginaIngreso'));
+const PaginaRegistro = lazy(() => import('../paginas/publicas/PaginaRegistro'));
+const PaginaRecuperacion = lazy(() => import('../paginas/publicas/PaginaRecuperacion'));
+const PaginaCambioContrasena = lazy(() => import('../paginas/publicas/PaginaCambioContrasena'));
+const PaginaNoEncontrada = lazy(() => import('../paginas/publicas/PaginaNoEncontrada'));
+
+// Importaciones para páginas legales
+const PaginaTerminosCondiciones = lazy(() => import('../paginas/publicas/TerminosCondiciones'));
+const PaginaPoliticaPrivacidad = lazy(() => import('../paginas/publicas/PoliticaPrivacidad'));
+const PaginaPoliticaCookies = lazy(() => import('../paginas/publicas/PoliticaCookies'));
+
+// Importaciones lazy loading para páginas privadas
+const PaginaProcesoPago = lazy(() => import('../paginas/privadas/PaginaProcesoPago'));
+const PaginaPerfil = lazy(() => import('../paginas/privadas/PaginaPerfil'));
+const PaginaReservasUsuario = lazy(() => import('../paginas/privadas/PaginaReservasUsuario'));
+// 🔧 NUEVA IMPORTACIÓN PARA DETALLE DE RESERVA
+const PaginaDetalleReserva = lazy(() => import('../paginas/privadas/PaginaDetalleReserva'));
+
+// Importaciones para las páginas de resultado de pago
+const PaginaReservaExitosa = lazy(() => import('../paginas/privadas/PaginaReservaExitosa'));
+const PaginaPagoFallido = lazy(() => import('../paginas/privadas/PaginaPagoFallido'));
+
+const RutasPublicas = () => {
+  return (
+    <Routes>
+      {/* Rutas públicas */}
+      <Route path="inicio" element={<PaginaInicio />} />
+      <Route path="tours" element={<PaginaTours />} />
+      <Route path="tours/:idTour" element={<PaginaDetalleTour />} />
+      <Route path="sedes" element={<PaginaSedes />} />
+      <Route path="sobre-nosotros" element={<PaginaSobreNosotros />} />
+      <Route path="contacto" element={<PaginaContacto />} />
+      <Route path="preguntas-frecuentes" element={<PaginaPreguntas />} />
+      <Route path="conservacion" element={<PaginaConservacion />} />
+
+      {/* Rutas de autenticación */}
+      <Route path="ingresar" element={<PaginaIngreso />} />
+      <Route path="registrarse" element={<PaginaRegistro />} />
+      <Route path="recuperar-contrasena" element={<PaginaRecuperacion />} />
+      <Route path="cambiar-contrasena" element={<PaginaCambioContrasena />} />
+      
+      {/* Rutas para páginas legales */}
+      <Route path="terminos" element={<PaginaTerminosCondiciones />} />
+      <Route path="privacidad" element={<PaginaPoliticaPrivacidad />} />
+      <Route path="cookies" element={<PaginaPoliticaCookies />} />
+
+      {/* Rutas privadas protegidas por autenticación */}
+      <Route element={<RutasPrivadas />}>
+        <Route path="proceso-pago" element={<PaginaProcesoPago />} />
+        <Route path="perfil" element={<PaginaPerfil />} />
+        <Route path="mis-reservas" element={<PaginaReservasUsuario />} />
+        {/* 🔧 NUEVA RUTA PARA DETALLE DE RESERVA */}
+        <Route path="reservas/:id" element={<PaginaDetalleReserva />} />
+        
+        {/* Rutas para los resultados de pago */}
         <Route path="reserva-exitosa" element={<PaginaReservaExitosa />} />
         <Route path="pago-fallido" element={<PaginaPagoFallido />} />
       </Route>
